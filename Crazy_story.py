85e6b7e8006c59b48953b874b962d247b430811d
mateo208 <mateo20818@gmail.com>
2023-03-06 22:42:11 -0500

    mi primer proyecto en python

---
+++ Crazy_story.py
@@ -0,0 +1,7 @@
+print("¡Bienvenidos al viaje en globo!")
+nombre = input("Por favor, introduce un nombre: ")
+verbo = input("Por favor, introduce un verbo en pasado: ")
+adjetivo = input("Por favor, introduce un adjetivo: ")
+lugar = input("Por favor, introduce un lugar: ")
+
+print(f"Había una vez un aventurero llamado {nombre}. Él siempre había soñado con {verbo} en un globo y finalmente, su sueño se hizo realidad. El globo era {adjetivo} y podía viajar a cualquier lugar que quisiera. {nombre} decidió ir al {lugar} y cuando llegó, descubrió que había un tesoro escondido allí. ¡Qué emocionante fue el viaje en globo de {nombre}!")
